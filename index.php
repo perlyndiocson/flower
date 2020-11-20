869f5581aa83ba05c57ac259354c2b5ab0d9bf79
perlyndiocson <60122597+perlyndiocson@users.noreply.github.com>
2020-11-20 23:58:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   </head>
   <body>
    
-    <center><h1> HELLO WELCOME TO FLOOM !!</h1></center>
+    <center><h1> HELLO WELCOME TO FLOOM !!</H1></center>
     <p>Why we champion local independent florists</p>
     <p> it's not always easy to find a beautiful crafted
 bouquets when ordering flowers online
@@ -22,6 +22,52 @@ local florist make sending flowers a truly special occasion,
       every time. Now We Deliver gorceries and hampers!</p>
   </body>
 </html>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
+</head>
+<body>
+
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
+
+</body>
+</html>
+
+
 
     
 
